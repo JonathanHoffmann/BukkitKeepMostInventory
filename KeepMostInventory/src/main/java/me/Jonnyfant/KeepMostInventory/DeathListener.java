f32b01d8a521cb8e0839d2546a6746fde849339c
Jonathan Hoffmann <jonathan.hoffmann@gmx.net>
2023-03-26 00:28:41 +0100

    V2

--- KeepMostInventory/src/main/java/me/Jonnyfant/KeepMostInventory/DeathListener.java
+++ KeepMostInventory/src/main/java/me/Jonnyfant/KeepMostInventory/DeathListener.java
@@ -1,113 +1,125 @@
 package me.Jonnyfant.KeepMostInventory;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
-import java.util.ArrayList;
+import java.util.List;
+
+import static org.bukkit.enchantments.Enchantment.BINDING_CURSE;
+import static org.bukkit.enchantments.Enchantment.VANISHING_CURSE;
 
 public class DeathListener implements Listener {
-    public void onPlayerDeath(PlayerDeathEvent e)
-    {
-        if(e.getKeepInventory())
-        {
-            String lostItemsMessage = "You have lost the following items:";
-            Player p = e.getEntity();
-            Inventory inventory = p.getInventory();
-            ArrayList<Material> itemsToKeep = new ArrayList();
-            //ARMOR
-            //HELMET
-            itemsToKeep.add(Material.LEATHER_HELMET);
-            itemsToKeep.add(Material.CHAINMAIL_HELMET);
-            itemsToKeep.add(Material.IRON_HELMET);
-            itemsToKeep.add(Material.GOLDEN_HELMET);
-            itemsToKeep.add(Material.DIAMOND_HELMET);
-            itemsToKeep.add(Material.NETHERITE_HELMET);
-            itemsToKeep.add(Material.TURTLE_HELMET);
-            //CHEST
-            itemsToKeep.add(Material.LEATHER_CHESTPLATE);
-            itemsToKeep.add(Material.CHAINMAIL_CHESTPLATE);
-            itemsToKeep.add(Material.IRON_CHESTPLATE);
-            itemsToKeep.add(Material.GOLDEN_CHESTPLATE);
-            itemsToKeep.add(Material.DIAMOND_CHESTPLATE);
-            itemsToKeep.add(Material.NETHERITE_CHESTPLATE);
-            itemsToKeep.add(Material.ELYTRA);
-            //PANTS
-            itemsToKeep.add(Material.LEATHER_LEGGINGS);
-            itemsToKeep.add(Material.CHAINMAIL_LEGGINGS);
-            itemsToKeep.add(Material.IRON_LEGGINGS);
-            itemsToKeep.add(Material.GOLDEN_LEGGINGS);
-            itemsToKeep.add(Material.DIAMOND_LEGGINGS);
-            itemsToKeep.add(Material.NETHERITE_LEGGINGS);
-            //Boots
-            itemsToKeep.add(Material.LEATHER_BOOTS);
-            itemsToKeep.add(Material.CHAINMAIL_BOOTS);
-            itemsToKeep.add(Material.IRON_BOOTS);
-            itemsToKeep.add(Material.GOLDEN_BOOTS);
-            itemsToKeep.add(Material.DIAMOND_BOOTS);
-            itemsToKeep.add(Material.NETHERITE_BOOTS);
-            //TOOLS
-            //HOE
-            itemsToKeep.add(Material.WOODEN_HOE);
-            itemsToKeep.add(Material.STONE_HOE);
-            itemsToKeep.add(Material.IRON_HOE);
-            itemsToKeep.add(Material.GOLDEN_HOE);
-            itemsToKeep.add(Material.DIAMOND_HOE);
-            itemsToKeep.add(Material.NETHERITE_HOE);
-            //AXE
-            itemsToKeep.add(Material.WOODEN_AXE);
-            itemsToKeep.add(Material.STONE_AXE);
-            itemsToKeep.add(Material.IRON_AXE);
-            itemsToKeep.add(Material.GOLDEN_AXE);
-            itemsToKeep.add(Material.DIAMOND_AXE);
-            itemsToKeep.add(Material.NETHERITE_AXE);
-            //PICKAXE
-            itemsToKeep.add(Material.WOODEN_PICKAXE);
-            itemsToKeep.add(Material.STONE_PICKAXE);
-            itemsToKeep.add(Material.IRON_PICKAXE);
-            itemsToKeep.add(Material.GOLDEN_PICKAXE);
-            itemsToKeep.add(Material.DIAMOND_PICKAXE);
-            itemsToKeep.add(Material.NETHERITE_PICKAXE);
-            //SHOVEL
-            itemsToKeep.add(Material.WOODEN_SHOVEL);
-            itemsToKeep.add(Material.STONE_SHOVEL);
-            itemsToKeep.add(Material.IRON_SHOVEL);
-            itemsToKeep.add(Material.GOLDEN_SHOVEL);
-            itemsToKeep.add(Material.DIAMOND_SHOVEL);
-            itemsToKeep.add(Material.NETHERITE_SHOVEL);
-            //RANDOM TOOL
-            itemsToKeep.add(Material.FISHING_ROD);
-            itemsToKeep.add(Material.FLINT_AND_STEEL);
-            itemsToKeep.add(Material.SHEARS);
-            itemsToKeep.add(Material.SHULKER_BOX);
+    FileConfiguration config;
 
-            //Getting rid of stuff
-            p.setTotalExperience(p.getTotalExperience()/4);
-            for(ItemStack itemStack:inventory.getContents())
-            {
-                boolean keepThisStack = false;
-                for(int i = 0; i<itemsToKeep.size();i++)
-                {
-                    if(itemStack.getType().equals(itemsToKeep.get(i)))
-                    {
-                        keepThisStack=true;
-                        break;
-                    }
+    public DeathListener(FileConfiguration c) {
+        config=c;
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+
+
+        int xpPercent = config.getInt("% XP retained");
+        if (xpPercent < 0) {
+            xpPercent=0;
+        } else if (xpPercent > 100) {
+            xpPercent=100;
+        }
+
+        if (config.getBoolean("Enabled")) {
+
+            Player p = e.getEntity();
+
+            Boolean chat = config.getBoolean("Send Death Message");
+            Boolean curse = config.getBoolean("Handle Curses properly");
+            if (e.getKeepInventory()) {
+                if(chat) {
+                    p.sendMessage(ChatColor.RED + "You have lost the following items:");
                 }
-                if(!keepThisStack)
-                {
-                    inventory.remove(itemStack);
-                    p.getWorld().dropItemNaturally(p.getLocation(),itemStack);
-                    lostItemsMessage+="\n";
-                    lostItemsMessage+=itemStack.getAmount();
-                    lostItemsMessage+="\t";
-                    lostItemsMessage+=itemStack.getType().name();
+                boolean flagNothing = true;
+
+
+                Inventory inventory = p.getInventory();
+                //ArrayList<Material> itemsToKeep = new ArrayList();
+                List<String>itemsToKeep = config.getStringList("Items to keep");
+
+                //Getting rid of stuff
+
+                //XP
+                p.setExp(p.getExp() / 100*xpPercent);
+                p.setLevel(p.getLevel() / 100*xpPercent);
+                //p.setTotalExperience(p.getTotalExperience()/100*xpPercent);
+
+
+
+                //items
+                int pos = 0;
+                for (ItemStack itemStack : inventory.getContents()) {
+                    boolean keepThisStack = false;
+                    if (itemStack != null && pos < 36) {
+                        if(curse)
+                        {
+                            if(itemStack.containsEnchantment(VANISHING_CURSE))
+                            {
+                                inventory.remove(itemStack);
+                            }
+                        }
+                        for (int i = 0; i < itemsToKeep.size(); i++) {
+                            String temp = String.valueOf(itemStack.getType());
+                            if (itemStack.getType().name().equals(itemsToKeep.get(i))) {
+                                keepThisStack = true;
+                                break;
+                            }
+                        }
+                        if (!keepThisStack) {
+                            flagNothing = false;
+                            inventory.remove(itemStack);
+                            p.getWorld().dropItemNaturally(p.getLocation(), itemStack);
+                            if (chat) {
+                                p.sendMessage(ChatColor.RED + (itemStack.getAmount() + " " + itemStack.getType().name()));
+                            }
+                        }
+                    }
+                    pos++;
+                }
+                if(curse) {
+                    PlayerInventory playerInventory = p.getInventory();
+                    ItemStack[] armor = playerInventory.getArmorContents();
+                    for(int i=0;i<armor.length;i++)
+                    {
+                        if(armor[i].containsEnchantment(VANISHING_CURSE))
+                        {
+                            armor[i]=new ItemStack(Material.AIR);
+                            p.sendMessage(ChatColor.RED + (armor[i].getAmount() + " " + armor[i].getType().name() + " was destroyed by Curse of Vanishing."));
+                        }
+                        else
+                        {
+                            if(armor[i].containsEnchantment(BINDING_CURSE))
+                            {
+                                armor[i]=new ItemStack(Material.AIR);
+                                flagNothing = false;
+                                p.getWorld().dropItemNaturally(p.getLocation(), armor[i]);
+                                if (chat) {
+                                    p.sendMessage(ChatColor.RED + (armor[i].getAmount() + " " + armor[i].getType().name()));
+                                }
+                            }
+                        }
+                    }
+                    playerInventory.setArmorContents(armor);
+
+                }
+                if (flagNothing&&chat) {
+                    p.sendMessage(ChatColor.GREEN + "Nothing");
                 }
             }
-            p.sendMessage(lostItemsMessage);
         }
     }
 }
--- KeepMostInventory/src/main/java/me/Jonnyfant/KeepMostInventory/KeepMostInventory.java
+++ KeepMostInventory/src/main/java/me/Jonnyfant/KeepMostInventory/KeepMostInventory.java
@@ -2,9 +2,126 @@ package me.Jonnyfant.KeepMostInventory;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KeepMostInventory extends JavaPlugin {
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new DeathListener(),this);
+        loadConfig();
+        getServer().getPluginManager().registerEvents(new DeathListener(getConfig()),this);
+    }
+
+    public void loadConfig() {
+        getConfig().options().header("Please be careful when editing items that players keep when dying. Copy the names from this list of internal IDs: https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Material.html");
+        getConfig().addDefault("Enabled", true);
+        getConfig().addDefault("Handle Curses properly", true);
+        getConfig().addDefault("% XP retained", 25);
+        getConfig().addDefault("Send Death Message", true);
+        getConfig().addDefault("Items to keep", generateItemsToKeep());
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    public List<String> generateItemsToKeep()
+    {
+        List<String> out = new ArrayList<String>() {
+        };
+        //ARMOR
+        //HELMET
+        out.add("LEATHER_HELMET");
+        out.add("CHAINMAIL_HELMET");
+        out.add("IRON_HELMET");
+        out.add("GOLDEN_HELMET");
+        out.add("DIAMOND_HELMET");
+        out.add("NETHERITE_HELMET");
+        out.add("TURTLE_HELMET");
+        //CHEST
+        out.add("LEATHER_CHESTPLATE");
+        out.add("CHAINMAIL_CHESTPLATE");
+        out.add("IRON_CHESTPLATE");
+        out.add("GOLDEN_CHESTPLATE");
+        out.add("DIAMOND_CHESTPLATE");
+        out.add("NETHERITE_CHESTPLATE");
+        out.add("ELYTRA");
+        //PANTS
+        out.add("LEATHER_LEGGINGS");
+        out.add("CHAINMAIL_LEGGINGS");
+        out.add("IRON_LEGGINGS");
+        out.add("GOLDEN_LEGGINGS");
+        out.add("DIAMOND_LEGGINGS");
+        out.add("NETHERITE_LEGGINGS");
+        //Boots
+        out.add("LEATHER_BOOTS");
+        out.add("CHAINMAIL_BOOTS");
+        out.add("IRON_BOOTS");
+        out.add("GOLDEN_BOOTS");
+        out.add("DIAMOND_BOOTS");
+        out.add("NETHERITE_BOOTS");
+        //TOOLS
+        //HOE
+        out.add("WOODEN_HOE");
+        out.add("STONE_HOE");
+        out.add("IRON_HOE");
+        out.add("GOLDEN_HOE");
+        out.add("DIAMOND_HOE");
+        out.add("NETHERITE_HOE");
+        //AXE
+        out.add("WOODEN_AXE");
+        out.add("STONE_AXE");
+        out.add("IRON_AXE");
+        out.add("GOLDEN_AXE");
+        out.add("DIAMOND_AXE");
+        out.add("NETHERITE_AXE");
+        //PICKAXE
+        out.add("WOODEN_PICKAXE");
+        out.add("STONE_PICKAXE");
+        out.add("IRON_PICKAXE");
+        out.add("GOLDEN_PICKAXE");
+        out.add("DIAMOND_PICKAXE");
+        out.add("NETHERITE_PICKAXE");
+        //SHOVEL
+        out.add("WOODEN_SHOVEL");
+        out.add("STONE_SHOVEL");
+        out.add("IRON_SHOVEL");
+        out.add("GOLDEN_SHOVEL");
+        out.add("DIAMOND_SHOVEL");
+        out.add("NETHERITE_SHOVEL");
+        //SWORD
+        out.add("WOODEN_SWORD");
+        out.add("STONE_SWORD");
+        out.add("IRON_SWORD");
+        out.add("GOLDEN_SWORD");
+        out.add("DIAMOND_SWORD");
+        out.add("NETHERITE_SWORD");
+        //RANDOM TOOL
+        out.add("FISHING_ROD");
+        out.add("FLINT_AND_STEEL");
+        out.add("SHEARS");
+        out.add("SHIELD");
+        out.add("CARROT_ON_A_STICK");
+        out.add("BOW");
+        out.add("CROSSBOW");
+        out.add("WARPED_FUNGUS_ON_A_STICK");
+        out.add("TRIDENT");
+        //ShulkerBoxes
+        out.add("SHULKER_BOX");
+        out.add("WHITE_SHULKER_BOX");
+        out.add("LIGHT_GRAY_SHULKER_BOX");
+        out.add("GRAY_SHULKER_BOX");
+        out.add("BLACK_SHULKER_BOX");
+        out.add("BROWN_SHULKER_BOX");
+        out.add("RED_SHULKER_BOX");
+        out.add("ORANGE_SHULKER_BOX");
+        out.add("YELLOW_SHULKER_BOX");
+        out.add("LIME_SHULKER_BOX");
+        out.add("GREEN_SHULKER_BOX");
+        out.add("CYAN_SHULKER_BOX");
+        out.add("LIGHT_BLUE_SHULKER_BOX");
+        out.add("BLUE_SHULKER_BOX");
+        out.add("PURPLE_SHULKER_BOX");
+        out.add("MAGENTA_SHULKER_BOX");
+        out.add("PINK_SHULKER_BOX");
+        return out;
     }
 }
